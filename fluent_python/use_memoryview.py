4d8019103c8c2bf9d806f0335c5d1be792169697
yhkl-dev <kaiyang939325@gmail.com>
2020-03-25 22:40:51 +0800

    update

--- fluent_python/use_memoryview.py
+++ fluent_python/use_memoryview.py
@@ -10,3 +10,23 @@ print(memv_oct.tolist())
 print(numbers)
 memv_oct[5] = 4
 print(numbers)
+
+
+def calculate_element(sequence):
+    res = {}
+    for e in set(sequence):
+        res[e] = sequence.count(e)
+    return res
+
+
+def compare_number(x, y):
+    if bool(int(x) - int(y)):
+        return False
+    return True
+
+
+def compare_number_2(x, y):
+    if bool(int(x) ^ int(y)):
+        return False
+    return True
+
